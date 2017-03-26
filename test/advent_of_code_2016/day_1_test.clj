ebf5d0b44981ff23222844115b4b7d9fdebf8c5b
Stuart White <s.white.1@elsevier.com>
2017-03-26 11:30:06 +0100

    Remove spurious square brackets in test template

--- test/advent_of_code_2016/day_1_test.clj
+++ test/advent_of_code_2016/day_1_test.clj
@@ -2,16 +2,16 @@
   (:require [advent-of-code-2016.day-1 :refer :all]
             [clojure.test :refer :all]))
 
-(deftest distance-to-destination-one-given-example-test-case-then-expected-result []
+(deftest distance-to-destination-one-given-example-test-case-then-expected-result
   (is (= (distance-to-destination (create-state ["R2" "L3"])           all-steps-completed?) 5))
   (is (= (distance-to-destination (create-state ["R2" "R2" "R2"])      all-steps-completed?) 2))
   (is (= (distance-to-destination (create-state ["R5" "L5" "R5" "R3"]) all-steps-completed?) 12)))
 
-(deftest solution-part-one-then-expected-result []
+(deftest solution-part-one-then-expected-result
   (is (= (solution-part-one) 242)))
 
-(deftest distance-to-destination-two-given-example-test-case-then-expected-result []
+(deftest distance-to-destination-two-given-example-test-case-then-expected-result
   (is (= (distance-to-destination (create-state ["R8" "R4" "R4" "R8"]) visited-twice?) 4)))
 
-(deftest solution-part-two-then-expected-result []
+(deftest solution-part-two-then-expected-result
   (is (= (solution-part-two) 150)))
